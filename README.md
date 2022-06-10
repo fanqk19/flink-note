51b02624694ad73e178f4133656de19787fa4d66
wxwmd <706461857@qq.com>
2022-06-10 20:54:53 +0800

    flink全窗口函数探究√ todo： flink keyed stream窗口函数探究

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # flink-coding
-给出flink各功能的代码实现，帮助开发者快速学习flink
+**基于flink1.15，给出flink各功能的代码实现，帮助开发者快速学习flink**。
+
+<!-- the line below needs to be an empty line C: (its because kramdown isnt
+     that smart and dearly wants an empty line before a heading to be able to
+     display it as such, e.g. website) -->
+
+
+[comment]: <> (![Java CI]&#40;https://github.com/iluwatar/java-design-patterns/workflows/Java%20CI/badge.svg&#41;)
+[![License MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/iluwatar/java-design-patterns/master/LICENSE.md)
+
+[comment]: <> ([![Lines of Code]&#40;https://sonarcloud.io/api/project_badges/measure?project=iluwatar_java-design-patterns&metric=ncloc&#41;]&#40;https://sonarcloud.io/dashboard?id=iluwatar_java-design-patterns&#41;)
+
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+# 介绍
+
+Apache Flink 是一个在有界数据流和无界数据流上进行有状态计算分布式处理引擎和框架。Flink 设计旨在所有常见的集群环境中运行，以任意规模和内存级速度执行计算。
+
+作为apache的顶级项目之一，flink的更新迭代非常快。短短一年事件内，就完成了从version 1.12到version 1.15的更新迭代。与此同时，很多api接口、架构方式等都发生了变化。
+我注意到现在市面上的flink学习资料都比较陈旧。例如说，很多教程中自定义数据源还在使用已经过时的`sourceFunction`。因此，本项目旨在探究最新的flink api接口、其背后的架构。
+
+![](./images/framework.jpg)
+
+# Getting started
+本项目基于maven构建，在这里示范一下使用idea构建项目的流程：
+1. 使用idea下载项目：
+![](./images/build/step1.png)
+2. 在idea右侧找到maven选项卡，打开`flink-coding`模块，先点clean，clean完成之后点compile
+![](./images/build/step2.png)
+3. 在maven构建完成之后，项目中的代码就可以运行了。
+
+# 参与贡献
+如果想要参与贡献，欢迎pull requests，也可以在issues区提出问题，我下班之后都会看的。
+
+# License
+本项目使用MIT license。
--- flink-window/src/main/java/com/wxwmd/window/windows/TumblingWindow.java
+++ flink-window/src/main/java/com/wxwmd/window/windows/TumblingWindow.java
@@ -2,29 +2,40 @@ package com.wxwmd.window.windows;
 
 import com.wxwmd.util.model.UserAction;
 import com.wxwmd.util.model.UserEvent;
-import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.RichAggregateFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
 import static com.wxwmd.window.watermark.AssignWatermark.getFileStreamWithWatermark;
 
 /**
  * @author wxwmd
  * @description 滚动窗口
+ * 演示如何计算每个滚动窗口内有多少个登录事件
  */
 public class TumblingWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(2);
 
         // 得到带有事件时间和watermark的source流
         DataStream<UserEvent> source = getFileStreamWithWatermark(env);
 
-        DataStream<Integer> loginCountStream = loginCount(source);
+        DataStream<Object> loginCountStream = loginCount(source);
 
+        /*
+        这里想说一下，如果多次运行程序，会看到一个非常有趣的现象：
+        window: [1654688060, 1654688070]对应的count一会是0，一会是1
+        这是因为我们是分布式读取文件，如果事件时间很大的那些事件先被读取，那么在doris,LOGIN,1654688067被读取之前，
+        这些事件时间很大的事件就把水位线设置为>1654688070，因此window: [1654688060, 1654688070]已经被关闭了，此时count是0
+        如果doris,LOGIN,1654688067先被读取，之后才读取到那些事件时间很大的事件，此时关口关闭，计算count结果为1
+         */
         loginCountStream.print("login count per tumbling window: ");
 
         env.execute();
@@ -36,26 +47,60 @@ public class TumblingWindow {
      * @param source 已经带有timestamp和watermark的数据流
      * @return 每个窗口内的登录人数
      */
-    public static DataStream<Integer> loginCount(DataStream<UserEvent> source){
-        SingleOutputStreamOperator<Integer> loginCountStream = source.windowAll(TumblingEventTimeWindows.of(Time.milliseconds(10L)))
+    public static DataStream<Object> loginCount(DataStream<UserEvent> source){
+//        AggregateFunction
+//        SingleOutputStreamOperator loginCountStream = source.windowAll(TumblingEventTimeWindows.of(Time.milliseconds(10L)))
+//                .aggregate(new LoginCountAggregateFunction());
 
-                .aggregate(new LoginCountAggregateFunction());
+//        ProcessFunction
+        SingleOutputStreamOperator loginCountStream = source.windowAll(TumblingEventTimeWindows.of(Time.milliseconds(10L)))
+                .process(new LoginCountProcessFunction());
 
         return loginCountStream;
     }
 
+    /**
+     * 使用ProcessFunction计算一个窗口内有多少登录事件
+     */
+    public static class LoginCountProcessFunction extends ProcessAllWindowFunction<UserEvent, String, TimeWindow>{
 
+        @Override
+        public void process(ProcessAllWindowFunction<UserEvent, String, TimeWindow>.Context context, Iterable<UserEvent> elements, Collector<String> out) throws Exception {
+            long startTime = context.window().getStart();
+            long endTime = context.window().getEnd();
+
+            int count = 0;
+            for (UserEvent userEvent : elements) {
+                if (userEvent.getAction().equals(UserAction.LOGIN)) {
+                    count += 1;
+                }
+            }
+            String loginCount = String.format("window: [%d, %d]: login count: %d", startTime, endTime, count);
+            out.collect(loginCount);
+        }
+    }
 
     /**
-     * 计算一个窗口内有多少登录事件
+     * 使用AggregateFunction计算一个窗口内有多少登录事件
      */
     public static class LoginCountAggregateFunction extends RichAggregateFunction<UserEvent, Integer, Integer> {
 
+        /**
+         * 创建累加器，初始值为0
+         * @return 0
+         */
         @Override
         public Integer createAccumulator() {
             return 0;
         }
 
+        /**
+         * 当元素落入窗口中，对累加器的值进行更新
+         * 我们这里进行了一个简单的判断，如果UserEvent是登录活动，那么累加器+1
+         * @param value 落在窗口内的元素
+         * @param accumulator old累加器
+         * @return 累加器的新值
+         */
         @Override
         public Integer add(UserEvent value, Integer accumulator) {
             if (value.getAction().equals(UserAction.LOGIN)){
@@ -64,6 +109,11 @@ public class TumblingWindow {
             return accumulator;
         }
 
+        /**
+         * 窗口被触发时，AggregateFunction返回的计算结果
+         * @param accumulator 累加器
+         * @return 窗口的计算结果
+         */
         @Override
         public Integer getResult(Integer accumulator) {
             return accumulator;
@@ -74,4 +124,6 @@ public class TumblingWindow {
             return null;
         }
     }
+
+
 }
